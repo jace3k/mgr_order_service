866167e770332bd013c09e81a5b7ac7d46ec7b45
Jacek <jacek.piszczek@nokia.com>
2020-05-19 23:15:19 +0200

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,13 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 orders = []
-(1..1000).each { |id| (1..((id % 10) + 15)).each {|order| orders.append({
-  quantity: rand(5..50),
-  product_id: id
-})} }
+(1..1000).each { |id|
+  (1..((id % 10) + 15)).each { |order|
+    orders.append({
+      quantity: 50,
+      product_id: id,
+    })
+  }
+}
 
 Order.create(orders)
